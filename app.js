e37b90065b3617a51c1bcb65538bf4084a8cc6cb
Aleksey Politov <aleks.politov@gmail.com>
2015-04-16 19:01:04 +0300

    Sample initial release

---
+++ app.js
@@ -0,0 +1,28 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/// <reference path="typings/tsd.d.ts" />
+var $ = require("jquery");
+var SampleModule;
+(function (SampleModule) {
+    var Greeter = (function () {
+        function Greeter(message) {
+            this.greeting = message;
+        }
+        Greeter.prototype.greet = function () {
+            //var data: string = <string><any>this.greeting;
+            $(".log").text(this.greeting);
+            return this.greeting;
+        };
+        Greeter.GLOBAL = "GLOBAL2";
+        return Greeter;
+    })();
+    SampleModule.Greeter = Greeter;
+})(SampleModule || (SampleModule = {}));
+var sample = new SampleModule.Greeter("text");
+sample.greet();
+
+},{"jquery":2}],2:[function(require,module,exports){
+
+},{}]},{},[1])
+
+
+//# sourceMappingURL=app.js.map
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp"),
+	browserify = require('browserify'),
+	debowerify = require('debowerify'),
+	tsify = require('tsify'),
+	exorcist = require('exorcist'),
+	source = require('vinyl-source-stream'),
+	gutil = require('gulp-util'),
+	watchify = require('watchify');
+
+var config = {
+	paths: {
+		ts: ["./*.ts", "typings/**/*.ts", "!node_modules/**/*.ts"],
+		jsresult: 'main.js',
+		app: __dirname,
+		pub: __dirname
+	}
+};
+
+gulp.task('compile-ts', function() {
+	var opts = {
+		basedir: config.paths.app,
+		debug: true
+	};
+	
+	var b = browserify(opts)
+		.add(config.paths.app + '/main.ts')
+		.ignore('jquery')
+		.ignore('angular')
+		.plugin(tsify, {
+			noImplicitAny: true,
+			target: 'es5'
+		})
+		.transform(debowerify);
+
+	return b.bundle()
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(exorcist(config.paths.pub + '/app.js.map'))
+		.pipe(source('app.js'))
+		.pipe(gulp.dest(config.paths.pub));
+
+});
+
+gulp.task("watch", function() {
+	gulp.watch(config.paths.ts, ["compile-ts"]);
+});
+
+gulp.task('default', ['watch']);
