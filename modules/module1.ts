cf3e4625125663fe9f7a21885c78c54d1e9851c1
Aleksey Politov <aleks.politov@gmail.com>
2015-06-26 11:31:17 +0300

    Gulp debugging version

--- modules/module1.ts
+++ modules/module1.ts
@@ -1,19 +1,19 @@
 
-	 export class UserModel {
-		private name: string = "";
-		private id: number = 0;
-		constructor(name: string, id: number) {
-			this.id = id;
-			this.name = name;
-		}
-		toString():string {
-			return this.id + "|" + this.name;
-		}
+export class UserModel {
+	private name: string = "";
+	private id: number = 0;
+	constructor(name: string, id: number) {
+		this.id = id;
+		this.name = name;
 	}
-	
-	export function SampleFunc(){
-		return "";
+	toString(): string {
+		return this.id + "|" + this.name;
 	}
+}
+
+export function SampleFunc() {
+	return "";
+}
 
 
 	
